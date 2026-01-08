13eb40bb8b1b257348775d004ad853c85df1909d
Thejus Rajendran <thejusr1999@icloud.com>
2026-01-08 19:41:41 +0530

    docker file and docker hot reload

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+	.get('/', () => 'Hello Elysia Hot Relaod')
+	.listen(3000)
+
+console.log(
+	`🦊 Elysia 2 is running at ${app.server?.hostname}:${app.server?.port}`
+)
